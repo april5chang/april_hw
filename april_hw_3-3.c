1e4338e1066df1a7496b21424db75b6c24843179
april5chang <42896767+april5chang@users.noreply.github.com>
2018-09-09 21:28:34 +0800

    april debug

--- april_hw_3-3.c
+++ april_hw_3-3.c
@@ -1,14 +1,25 @@
 #include <stdio.h> 
 #include <math.h>
 
-int main()
+int main(void)
 {
 	int i = 0, num = 0;
 	float sqrtN = 0;
 	
-	printf("Please input number: ");
-	scanf("%d", &num);
+	while(1)
+	{
+		printf("Please input number: ");
+		scanf("%d", &num);
 	
+		if(num <= 0)
+		{
+			printf("Please input positive integer: ");
+			fflush(stdin);
+		}
+		else
+			break;
+ 	}
+ 	
 	sqrtN = sqrt(num);
 	
 	for(i = 2 ; i < sqrtN ; i++)
